7f2ebf89b1d15d94d94b62e537670be3d2fe3208
pockypoem <jeremyas.cornelis@gmail.com>
2024-02-15 12:26:54 +0700

    feat: add controlled Elements and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Trash2 } from 'lucide-react'
+import { useState } from 'react';
 
 /* eslint-disable react/prop-types */
 const initialItems = [
@@ -23,13 +24,35 @@ function Logo() {
 }
 
 function Form() {
+
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {description, quantity, packed: false, id:Date.now()}
+    console.log(newItem);
+
+    setDescription('')
+    setQuantity(1)
+    
+  }
+
+
   return (
-    <form className="flex mt-6 text-center justify-center backdrop-sepia-0 bg-white/30 py-3 px-3 rounded-lg">
+    <form className="flex mt-6 text-center justify-center backdrop-sepia-0 bg-white/30 py-3 px-3 rounded-lg" onSubmit={handleSubmit}>
       <h3 className="py-2 text-lg font-semibold mr-4">What do you need for your trip? 😍</h3>
-      <select className="mr-2 rounded-md px-2 py-1 bg-slate-200">
+      <select className="mr-2 rounded-md px-2 py-1 bg-slate-200" value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         { Array.from({length: 20}, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>) }
       </select>
-      <input type="text" placeholder="Item..." className="rounded-md bg-slate-100 px-2 mr-2"/>
+      <input 
+        type="text" 
+        placeholder="Item..." 
+        value={description}
+        className="rounded-md bg-slate-100 px-2 mr-2"
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button className="bg-green-600 rounded-lg py-2 px-2 text-white">Add</button>
     </form>
   )
@@ -52,7 +75,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li className='flex items-center space-x-3 backdrop-blur-0 bg-yellow-200/30 px-3 py-3 rounded-xl text-lg'>
-      <span>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
         {item.quantity} {item.description}
       </span>
       <button><Trash2 color="#ff0000" size={20}/></button>
