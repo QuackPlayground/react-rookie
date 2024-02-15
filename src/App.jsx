35cadb25d3572cf4d4ed1c55270ec18812f9b692
pockypoem <jeremyas.cornelis@gmail.com>
2024-02-15 11:45:01 +0700

    initialization & instalation proj

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
+
+export default function App() {
+  return(
+    <div className="app h-screen w-screen bg-gradient-to-r from-blue-200 to-cyan-200">
+      <h1 className="justify-center pt-4 text-center text-4xl font-semibold">🏝️ Letz Trip 💼</h1>
+    </div>
+  )
+}
