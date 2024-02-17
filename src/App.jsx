7c11de03b78c0e2d3648bf1ae38b3a6f13535192
pockypoem <jeremyas.cornelis@gmail.com>
2024-02-17 17:12:01 +0700

    finished accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import { useState } from "react";
 import "./index.css";
 
 const faqs = [
@@ -21,11 +24,36 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs}/>
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({data}) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} key={i}/>)}
+    </div>
+  )
+}
+
+
+function AccordionItem({ num, title, text }) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen(isOpen => !isOpen);
+  }
+  
+  return (
+    <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon"> {isOpen ? '-' : '+'} </p>
+      
+      { isOpen && <div className="content-box">{text}</div> }
+
+    </div>
+  )
 }
