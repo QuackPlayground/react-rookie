3add1c910df1c95973458f581d1e6191d80a255d
pockypoem <jeremyas.cornelis@gmail.com>
2024-02-21 14:20:18 +0700

    feat: adding UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
 
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑"
+]
+
 export default function App() {
   return(
     <div>
-      <h1>Haloo</h1>
+      <Steps />
+    </div>
+  )
+}
+
+
+function Steps() {
+  return(
+    <div>
+
+      <button className="close">
+        &times;
+      </button>
+      
+      <div className="steps">
+
+        <div className="numbers">
+          <div className="active">1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <p className="message">
+          Step: halo this is step 
+        </p>
+
+        <div className="buttons">
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+            Previous 
+          </button>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+            Next
+          </button>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
