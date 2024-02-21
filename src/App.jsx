5422d1fabb4edef20cee67616c1fad35149e3546
pockypoem <jeremyas.cornelis@gmail.com>
2024-02-21 14:58:26 +0700

    feat: add functionality to step

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -16,6 +16,19 @@ export default function App() {
 
 
 function Steps() {
+
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if(step > 1) setStep((curStep) => curStep - 1)
+  }
+
+  function handleNext() {
+    if(step < 3) {
+      setStep((curStep) => curStep + 1)
+    }
+  }
+
   return(
     <div>
 
@@ -26,20 +39,20 @@ function Steps() {
       <div className="steps">
 
         <div className="numbers">
-          <div className="active">1</div>
-          <div>2</div>
-          <div>3</div>
+          <div className={step >= 1 ? 'active': ''}>1</div>
+          <div className={step >= 2 ? 'active': ''}>2</div>
+          <div className={step >= 3 ? 'active': ''}>3</div>
         </div>
 
         <p className="message">
-          Step: halo this is step 
+          Step {step}: {messages[step - 1]}
         </p>
 
         <div className="buttons">
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
             Previous 
           </button>
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
             Next
           </button>
         </div>
